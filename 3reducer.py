d79e2f85c620c93dbecef9e61ae1e51c7abb81cd
StephenBurke <42814417+StephenBurke@users.noreply.github.com>
2020-02-12 14:24:24 -0600

    finished the quiz

--- 3reducer.py
+++ 3reducer.py
@@ -5,24 +5,24 @@ thisKey = ""
 thisValue = 0.0
 
 for line in s:
-  #data = line.strip().split('\t')
-  paymentType = data
+  data = line.strip().split('\n')
 
-  if paymentType != thisKey:
+
+  if data != thisKey:
     if thisKey:
       # output the last key value pair result
-      r.write(thisKey + '\t' + str(thisValue)+'\n')
+      r.write(str(thisKey) + '\t' + str(thisValue)+'\n')
 
     # start over when changing keys
-    thisKey = paymentType
+    thisKey = data
     thisValue = 0.0
 
   # apply the aggregation function
   #thisValue += float(amount)
-  thisValue += 1
+  thisValue +=1
 
 # output the final entry when done
-r.write(thisKey + '\t' + str(thisValue)+'\n')
+r.write(str(thisKey) + '\t' + str(thisValue)+'\n')
 
 s.close()
 r.close()
